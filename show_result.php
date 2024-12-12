7aa63cff732cec2e67664801d5ad1c4bf64111e2
Muhammad Abubakar <163642750+sheikh-abubakar@users.noreply.github.com>
2024-12-12 14:52:18 +0500

    result succefuly added

--- show_result.php
+++ show_result.php
@@ -1,5 +1,6 @@
 <?php
-include 'config1.php'; // Include the database connection
+// Include the database connection file
+include 'config1.php';
 
 // Fetch all students and their results
 $sql = "SELECT PERSON.PERSON_ID, PERSON.FNAME, PERSON.LNAME, PERSON.EMAIL, 
@@ -13,22 +14,37 @@ $result = $conn->query($sql); // Execute the query
 
 // Check if any results were returned
 if ($result->num_rows > 0): ?>
-    <?php while($row = $result->fetch_assoc()): ?>
-        <tr>
-            <td><?php echo $row['PERSON_ID']; ?></td>
-            <td><?php echo $row['FNAME']; ?></td>
-            <td><?php echo $row['LNAME']; ?></td>
-            <td><?php echo $row['EMAIL']; ?></td>
-            <td><?php echo $row['DEGREE']; ?></td>
-            <td><?php echo $row['DEPT_CODE']; ?></td>
-            <td><?php echo $row['SUB']; ?></td>
-            <td><?php echo $row['MARKS']; ?></td>
-        </tr>
-    <?php endwhile; ?>
+    <h1>Student Results</h1>
+    <table>
+        <thead>
+            <tr>
+                <th>Person ID</th>
+                <th>First Name</th>
+                <th>Last Name</th>
+                <th>Email</th>
+                <th>Degree</th>
+                <th>Department Code</th>
+                <th>Subject</th>
+                <th>Marks</th>
+            </tr>
+        </thead>
+        <tbody>
+            <?php while($row = $result->fetch_assoc()): ?>
+                <tr>
+                    <td><?php echo $row['PERSON_ID']; ?></td>
+                    <td><?php echo $row['FNAME']; ?></td>
+                    <td><?php echo $row['LNAME']; ?></td>
+                    <td><?php echo $row['EMAIL']; ?></td>
+                    <td><?php echo $row['DEGREE']; ?></td>
+                    <td><?php echo $row['DEPT_CODE']; ?></td>
+                    <td><?php echo $row['SUB']; ?></td>
+                    <td><?php echo $row['MARKS']; ?></td>
+                </tr>
+            <?php endwhile; ?>
+        </tbody>
+    </table>
 <?php else: ?>
-    <tr>
-        <td colspan="8">No results found</td>
-    </tr>
+    <h1>No results found</h1>
 <?php endif; ?>
 
 <?php
